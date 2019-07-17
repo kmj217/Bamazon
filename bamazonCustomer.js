e3fc10d6bc4c7d1db4b481680a838a4d1b6719ba
KMJ217 <kmj217@hotmail.com>
2019-07-17 13:56:51 -0400

    customer works

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,108 +3,102 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 var table = require("console.table");
 
-// establish
+// create connection
 var connection = mysql.createConnection({
-	host: "localhost",
-	port: 3306,
+  host: "localhost",
+  port: 3306,
 
-	user: "root",
+  user: "root",
 
-	password: "Shift217!!",
-    database: "bamazondb"
-
+  password: "Shift217!!",
+  database: "bamazondb"
 });
 
 function allItems() {
-	connection.connect(function(err) {
-
-		connection.query("SELECT * FROM Products", function(err, res) {
-		if (err) throw err
-		console.table(res , "\n");
-		buyItem();
-		});
-	});
-
+  connection.connect(function(err) {
+    connection.query("SELECT * FROM Products", function(err, res) {
+      if (err) throw err;
+      console.table(res, "\n");
+      buyItem();
+    });
+  });
 }
 allItems();
 
 function buyItem() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "id",
+        message:
+          "Please enter the Item ID of the product you would like to buy.\n",
+        validate: function(value) {
+          if (!isNaN(value) && value < 11) {
+            return true;
+          }
+          return false;
+        }
+      },
 
-	inquirer.prompt([
+      {
+        type: "input",
+        name: "amount",
+        message: "How many units of the product would you like to buy? \n",
+        validate: function(value) {
+          if (!isNaN(value)) {
+            return true;
+          }
+          return false;
+        }
+      }
+    ])
+    .then(function(answer) {
+      var userId = answer.id;
+      console.log("Chosen item number: ", userId);
 
-		{
-		 type: "input",
-		 name: "id",
-		 message: "Please enter the Item ID of the product you would like to buy.\n",
-		 validate: function(value) {
-		 	if (!isNaN(value) && value < 11) {
-		 		return true;
-		 	}
-		 	return false;
-		 }
-		},
+      var userAmount = answer.amount;
+      console.log("Chosen amount from stock: ", userAmount, "\n");
 
-		{
-		 type: "input",
-		 name: "amount",
-		 message: "How many units of the product would you like to buy? \n",
-		 validate: function(value) {
-		 	if (!isNaN(value)) {
-		 		return true;
-		 	}
-		 	return false;
-			}
-		}
+      connection.query(
+        "SELECT * FROM products WHERE ?",
+        [{ Item: userId }],
+        function(err, res) {
+          if (err) throw err;
 
-		]).then(function(answer) {
+		  var currentAmount = res[0].Stock;
+		  var price = res[0].Price;
+		  var remainingAmount = currentAmount - answer.amount;
+		  console.log("You selected the following: ")
+		  console.table(res);
 
-			var userId = answer.id;
-			console.log("Chosen item number: " , userId);
+          if (currentAmount > answer.amount) {
+            console.log("Amount Remaining: " + remainingAmount);
+            console.log("Total Cost: " + answer.amount * price + "\n");
 
-			var userAmount = answer.amount;
-			console.log("Chosen amount from stock: " , userAmount , "\n");
+            connection.query(
+              "UPDATE Products SET Stock=? WHERE Item=?",
+              [remainingAmount, answer.id],
 
-			connection.query("SELECT * FROM products WHERE ?", [{ Item : userId }], function(err, res) {
-				if (err) throw err;
-				
-				console.table(res);
-				var current_amount = res[0].Stock;
-				console.log("Current amount in stock: " , current_amount);
-				var price = res[0].Price;
-				var remaining_amount = current_amount - answer.amount;
-				console.log("Remaining amount in stock: " , remaining_amount);
+              
+            );
 
-				if(current_amount > answer.amount) {
+            connection.query("SELECT * FROM Products", function(err, res) {
+              console.log("This is the updated inventory of product items: ");
+              console.log(
+                "------------------------------------------------ \n"
+              );
+              console.table(res);
+              if (err) throw err;
+            });
+          } else {
+            console.log(
+              "Insufficient stock remaining. Please enter a new amount."
+            );
+          }
 
-					console.log("Amount Remaining: " + remaining_amount);
-					console.log("Total Cost: " + (answer.amount * price) + "\n");
-
-					connection.query("UPDATE Products SET Stock=? WHERE Item=?",
-                    [
-                    remaining_amount, answer.id
-                    ],
-
-					
-						function(err, res){
-							console.table(res);
-							if (err) throw err;
-						});
-
-					connection.query("SELECT * FROM Products", function(err, res) {
-
-						console.log("This is the updated inventory of product items: ");
-						console.log("------------------------------------------------ \n");
-						console.table(res);
-						if (err) throw err;
-					});
-
-				} else {
-					console.log("Insufficient stock remaining. Please enter a new amount.");
-				}
-
-			connection.end();
-
-			});
-		})
-
+          connection.end();
+        }
+      );
+    });
 }
